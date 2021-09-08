0d26654fbcb57bc60e821375a41244655df5cb9a
Koppi Kolyvek <kk674@cornell.edu>
2021-09-08 10:51:29 -0700

    init github repo, add existing items

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,15 @@
+const { User } = require("../models");
+
+const resolvers = {
+  Query: {
+    users: async () => {
+      return User.find();
+    },
+
+    user: async (parent, { userId }) => {
+      return User.findOne({ _id: userId });
+    },
+  },
+};
+
+module.exports = resolvers;
