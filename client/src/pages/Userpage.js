d55f4e1a50178eda8ecbeecc5e82c3241c1cbe84
Koppi Kolyvek <kk674@cornell.edu>
2021-09-18 18:48:15 -0700

    began adding date picker / input areas

--- client/src/pages/Userpage.js
+++ client/src/pages/Userpage.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import './Userpage.css';
+import "./Userpage.css";
 import Userchart from "../Components/Chart/Userchart.tsx";
 import Userdatainput from "../Components/Userdatainput/Userdatainput.js";
 
@@ -9,45 +9,49 @@ export default function Userpage() {
   const [showCalories, setShowCalories] = useState(true);
   const [showWeight, setShowWeight] = useState(true);
 
+  return (
+    <div>
+      <div className="pre-chart-wrapper">
+        <div className="pre-chart-data-checkboxes-wrapper">
+          <strong>Data: </strong>
 
-  return (<div>
-    <div className="pre-chart-wrapper">
-      <div className="pre-chart-data-checkboxes-wrapper">
-        <strong>Data: </strong>
+          {/* CALORIES CHECKBOX */}
+          <input
+            type="checkbox"
+            id="calories"
+            checked={showCalories}
+            onClick={() => {
+              setShowCalories(!showCalories);
+            }}
+          />
+          <label for="calories">Calories</label>
 
-        {/* CALORIES CHECKBOX */}
-        <input
-          type="checkbox"
-          id="calories"
-          checked={showCalories}
-          onClick={() => {
-            setShowCalories(!showCalories)
-          }}
-        />
-        <label for="calories">Calories</label>
-
-        {/* WEIGHT CHECKBOX */}
-        <input
-          type="checkbox"
-          id="weight"
-          checked={showWeight}
-          onClick={() => {
-            setShowWeight(!showWeight)
-          }}
-        />
-        <label for="weight">Weight</label>
+          {/* WEIGHT CHECKBOX */}
+          <input
+            type="checkbox"
+            id="weight"
+            checked={showWeight}
+            onClick={() => {
+              setShowWeight(!showWeight);
+            }}
+          />
+          <label for="weight">Weight</label>
+        </div>
+      </div>
+      <Userchart showCalories={showCalories} showWeight={showWeight} />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          marginBottom: "10em",
+        }}
+      >
+        <div>
+          <input type="date" />
+        </div>
+        {/* <Userdatainput /> */}
       </div>
     </div>
-    <Userchart
-      showCalories={showCalories}
-      showWeight={showWeight}
-    />
-    <div style={{
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center"
-    }}>
-      {/* <Userdatainput /> */}
-    </div>
-  </div>)
+  );
 }
